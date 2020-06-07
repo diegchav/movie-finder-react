7145af9a8ca4cf701686d75c13b75375f15a2125
Diego Chavez <diego.ch.schez@gmail.com>
2020-06-07 18:16:43 -0500

    Updates

---
+++ precache-manifest.87cc279950fbecd1b4f73a0cdb7b627d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "83c28898ec1030f7f51a1a2ad54d05fa",
+    "url": "/movie-finder-react/index.html"
+  },
+  {
+    "revision": "48d21c82456bcdb7e5d1",
+    "url": "/movie-finder-react/static/css/main.606cfde1.chunk.css"
+  },
+  {
+    "revision": "e35af7435fe0e6f5c18d",
+    "url": "/movie-finder-react/static/js/2.f91654e9.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/movie-finder-react/static/js/2.f91654e9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "48d21c82456bcdb7e5d1",
+    "url": "/movie-finder-react/static/js/main.334e9f10.chunk.js"
+  },
+  {
+    "revision": "72c3156f76940dd37fce",
+    "url": "/movie-finder-react/static/js/runtime-main.eb5b9146.js"
+  }
+]);
